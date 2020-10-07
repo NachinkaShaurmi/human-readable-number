6f2af0cd66fbdcec1ef75efed25e5fcc7cf20620
Yuriy <agent250691@yandex.ru>
2020-10-07 22:57:01 +0300

    feat: human number solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    const to19 = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+    "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]; 
+
+    const to99 = [null, null, "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    let answer = "";
+
+    if (number < 20) return to19[number];
+    if (number >= 20 && number < 100) {
+        if (number % 10 === 0) return to99[number / 10];
+        else return `${to99[Math.floor(number / 10)]} ${to19[Math.floor(number % 10)]}`;
+    }
+    if (number >= 100) {
+        answer = `${to19[Math.floor(number / 100)]} hundred`;
+        if (number % 100 === 0) return answer;
+        if (number % 100 < 20) answer += " " + to19[number % 100];
+        if (number % 100 >= 20) {
+            if (number % 100 % 10 === 0) answer += " " + to99[number % 100 / 10];
+            else answer += ` ${to99[Math.floor(number % 100 / 10)]} ${to19[Math.floor(number % 100 % 10)]}`;
+        }
+    }
+    return answer;
 }
